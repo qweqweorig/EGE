6c9755e81f935ef974cbfb9299e9f568668cd669
guly2 <guly20071@gmail.com>
2024-10-08 18:43:20 +0300

    Перенос в апапапапку с 2м типом

--- 2task_type/2nd_type_2nd.py
+++ 2task_type/2nd_type_2nd.py
@@ -6,6 +6,6 @@ for x in range (0,2):
     for y in range(0,2):
         for z in range(0,2):
             for w in range(0,2):
-                F = int((x == not y) <= ((x and w) == (z and not w)))
+                F = int((x == (not y)) <= ((x and w) == (z and not w)))
                 #if F == 0:
                 print (x, y, z, w, F)
